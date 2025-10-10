3282e841a9eceb4e88cf4d975964adb28b8211f8
Johan <johan.francocampos@gmail.com>
2025-10-10 12:00:06 -0400

    deleted some lines and modified except print message

--- homework/HW - Franco-Campos - Personalized Greeting Program - CYSE 130 Fall 2025.py
+++ homework/HW - Franco-Campos - Personalized Greeting Program - CYSE 130 Fall 2025.py
@@ -11,15 +11,12 @@ def personalized_greeting():
     try:
         newage = str(int(age) + int(num)) # tries to add age and num to find age after num years
     except ValueError: #catches any value errors from trying to turn age and num into integers
-        print("\nNumber was not entered for either age or number") # tells user their error
+        print("\nNumber was not entered for either age or number. Please try again user!\n\n") # tells user their error
         personalized_greeting() # call its own function again till user puts in ints for both age and num
     else:
         # if no value error finish the program through giving user personalized greeting
         print(f"\nHello {name}!")
         print(f"You are {age} years old.")
         print(f"You will be {newage} years old in {num} years.")
-
-
-
 
 personalized_greeting()
